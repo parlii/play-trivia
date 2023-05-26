c0627371a887a735d170fd2b5e3d2d41498b3fb5
Parli Rizal <74726025+parlii@users.noreply.github.com>
2023-05-26 14:47:08 -0700

    blocked page

--- src/pages/blocked.tsx
+++ src/pages/blocked.tsx
@@ -1,10 +1,10 @@
-import styles from "@/styles/Home.module.css";
-
 export default function Blocked() {
   return (
-    <div>
-      <main className={styles.main}>
-        <h3>Too many requests. Access blocked. Try again later!</h3>
+    <div className="flex items-center justify-center bg-gray-100">
+      <main className="text-center p-4 bg-white rounded-lg shadow-md">
+        <h3 className="text-xl font-semibold text-red-600">
+          Too many requests. Access blocked. Try again later!
+        </h3>
       </main>
     </div>
   );
